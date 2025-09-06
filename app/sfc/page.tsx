5af1a858498d0b007a40c9a75ecf0376111f7b98
Diego França <diego.franca92@gmail.com>
2025-09-06 18:15:46 -0300

    inicial structure

---
+++ app/sfc/page.tsx
@@ -0,0 +1,32 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import ProjectCard from "@/components/card";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>São Francisco do Conde</span><br />
+        <span className={title({ color: "violet", size: "sm" })}>A Joia do Recôncavo</span>
+        <br />
+        <div className={subtitle({ class: "mt-4" })}>
+          Beautiful, fast and modern React UI library.
+        </div>
+      </div>
+
+      <div className="flex gap-3">
+        
+      </div>
+
+      <div className="mt-8">
+        <ProjectCard />
+      </div>
+    </section>
+  );
+}
---
+++ components/card.tsx
@@ -0,0 +1,23 @@
+import { Card, CardHeader, CardBody } from "@heroui/card";
+import Image from "next/image";
+
+export default function ProjectCard() {
+  return (
+    <Card className="py-4">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">Daily Mix</p>
+        <small className="text-default-500">12 Tracks</small>
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="/assets/sfc_orla.jpg"
+          width={270}
+          height={270}
+        />
+      </CardBody>
+    </Card>
+  );
+}
